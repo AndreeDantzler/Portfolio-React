9e4176050ca87078b6d1b39ab5cf3107af066491
Andree Dantzler <andree.balayssac@icloud.com>
2021-04-06 01:52:48 -0400

    Remove duplidate import

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./components/About/About.css";
 import "./components/Project/Project.css";
 import "./components/Navigation/Navigation.css";
 import Container from "react-bootstrap/Container";
-import { BrowserRouter, Switch, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Project from "./components/Project/Project";
 import About from "./components/About/About";
