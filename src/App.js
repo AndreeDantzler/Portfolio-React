44956b4bbfd14eb978f76566c1f1b09651db7bbb
Andree Dantzler <andree.balayssac@icloud.com>
2021-04-06 01:37:33 -0400

    using browswer routr

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./components/About/About.css";
 import "./components/Project/Project.css";
 import "./components/Navigation/Navigation.css";
 import Container from "react-bootstrap/Container";
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Project from "./components/Project/Project";
 import About from "./components/About/About";
@@ -19,7 +19,7 @@ import Home from "./components/Home/Home";
 function App() {
   return (
   <Container>
-    <HashRouter>
+    <Router basename={process.env.public_URL}>
     <Navigation/>
       <Header/>
           <Switch>
@@ -29,7 +29,7 @@ function App() {
             <Route exact path="/contact" component={Contact}/>
           </Switch>
       <Footer/>
-    </HashRouter>
+    </Router>
   </Container>
   );
 }
